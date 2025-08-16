ca67625bf08f8fa93390f063b3253230a592a99a
MSKarim <sajjatulkarim@gmail.com>
2025-08-17 01:46:08 +0600

    First commit without any change

---
+++ first.dart
@@ -0,0 +1,13 @@
+void main ()
+{
+  print("Hello");
+  int x =10;
+  double y = 10.5;
+  bool o = true;
+  num number = 100.9; //any number can be store in it
+  var pi = 3.14; // Var only use with data type
+  dynamic temp = true;
+  print(temp.runtimeType);// showing data type
+  int? i = null; // if  you want to make null you have to use (?) question mark
+
+}
