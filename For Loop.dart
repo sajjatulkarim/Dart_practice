877a996e33a1374362169c798160f77e6dd4c937
Sajjatul Karim <62699393+sajjatulkarim@users.noreply.github.com>
2025-09-29 14:20:11 +0600

    Add files via upload
    Initialized for loop in traditional and for each approach

---
+++ For Loop.dart
@@ -0,0 +1,16 @@
+void main()
+{
+//Traditional Approach. Print entire list or specific index items.
+  List loyatyscore = [5,6,7,5,10,9,8,6,5.5];
+  for (var i =0; i<=loyatyscore.length;i++) {
+    print ( [loyatyscore]);
+  }
+  // For each Approach. Here print entire list item
+  List weights = [22,34,53,24,75,29];
+  for (var weight in weights)
+    print (weight);
+}
+
+
+
+
