d3f4c258772fa78d0a53f03dbd7a733fe136c91d
Sajjatul Karim <62699393+sajjatulkarim@users.noreply.github.com>
2025-09-29 13:33:10 +0600

    Add files via upload
    Initialized LIST data structure in dart

---
+++ data structure.dart
@@ -0,0 +1,11 @@
+void main()
+{
+ // How data define in a logical form a term is used. It called data stucture. How to arrange data such a way that it can be update, retrieve data, and can be implement as per condition and logics.
+  // List is one of them. Here some example to deal with list data structure.
+  List loyatyscore = [5,6,7,5,10,9,8,6,5.5]; // simple store data in a array indecies.
+  List <int> extrarun = [3,5,1,4,0,0,3]; // Here the term read as( from the left side) List of integer extra run. Initialized datatype make sure the valid input should be stored.
+  List  tempataure =[22,43.6,true]; // This list contain any time of data.
+  final  List  temparature = [ 25,34,32,false]; // Now final keyword states that list item can not be changed
+  const List weight = <double> [23,34.2,44.9]; // Const = Constant key word also work as final keyword and on the right side a datatype has declared due to show here the list contain only float value.
+
+}
